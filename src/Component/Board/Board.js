e6454d9fd4a70e4f9932e0f860830289a2a2ef7a
Ronald Deguzman <bootcampcoding.ronnie@gmail.com>
2018-09-12 11:14:02 -0700

    completed game, functioning... now testing for bugs

--- src/Component/Board/Board.js
+++ src/Component/Board/Board.js
@@ -3,22 +3,12 @@ import React, { Component } from 'react';
 import Square from "../Square/Square"
 
 class Board extends React.Component {
-    handleClick(i) {
-      const squares = this.state.squares.slice();
-      if (calculateWinner(squares) || squares[i]) {
-        return;
-      }
-      squares[i] = this.state.xIsNext ? "X" : "O";
-      this.setState({
-      squares:squares,
-      xIsNext: !this.state.xIsNext
-    });
-    }
-
     renderSquare(i) {
-      return <Square value={this.props.squares[i]}
-                    onClick={() => this.props.onClick(i)}
-      />;
+      return (<Square 
+        value={this.props.squares[i]}
+        onClick={() => this.props.onClick(i)}
+      />
+      );
     }
   
     render() {  
@@ -44,25 +34,25 @@ class Board extends React.Component {
     }
   }
 
-  function calculateWinner(squares) {
-    const lines = [
-      [0, 1, 2],
-      [3, 4, 5],
-      [6, 7, 8],
-      [0, 3, 6],
-      [1, 4, 7],
-      [2, 5, 8],
-      [0, 4, 8],
-      [2, 4, 6],
-    ];
-    for (let i = 0; i < lines.length; i++) {
-      const [a, b, c] = lines[i];
-      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-        return squares[a];
-      }
-    }
-    return null;
-  }
+  // function calculateWinner(squares) {
+  //   const lines = [
+  //     [0, 1, 2],
+  //     [3, 4, 5],
+  //     [6, 7, 8],
+  //     [0, 3, 6],
+  //     [1, 4, 7],
+  //     [2, 5, 8],
+  //     [0, 4, 8],
+  //     [2, 4, 6],
+  //   ];
+  //   for (let i = 0; i < lines.length; i++) {
+  //     const [a, b, c] = lines[i];
+  //     if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+  //       return squares[a];
+  //     }
+  //   }
+  //   return null;
+  // }
 
 
 export default Board
